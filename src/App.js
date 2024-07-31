1a0521af7a385428956e5d64185665e548ad6d1f
Nithiya08 <146318148+Nithiya08@users.noreply.github.com>
2024-07-31 10:38:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,31 +30,31 @@ import TODo from './ToDo';
 function App() {
   return (
     <div className="App">
-      {/* <BoxComponent bgcolor='red' text='box2' height='120px' weight='120px'></BoxComponent>
+       <BoxComponent bgcolor='red' text='box2' height='120px' weight='120px'></BoxComponent>
       <BoxComponent bgcolor='blue' text='box1' height='100px' weight='100px'></BoxComponent>
       <Welcome name='John' color='blue'></Welcome>
-      <Welcome name='Jamesh' color='green'></Welcome> */}
-      {/* <SideEffect></SideEffect><br></br> */}
-      {/* <FetchProduct></FetchProduct> */}
-      {/* <FetchUser></FetchUser> */}
-      {/* <CardComp></CardComp> */}
-      {/* <Products></Products> */}
-      {/* <UserData></UserData> */}
-      {/* <LoginLogOut></LoginLogOut><br></br>
+      <Welcome name='Jamesh' color='green'></Welcome> 
+       <SideEffect></SideEffect><br></br> 
+       <FetchProduct></FetchProduct> <br></br>
+       <FetchUser></FetchUser> <br></br>
+       <CardComp></CardComp> <br></br>
+       <Products></Products> <br></br>
+       <UserData></UserData> <br></br>
+       <LoginLogOut></LoginLogOut><br></br>
       <HelloWorld></HelloWorld><br></br>
       <GreetUser name='John'></GreetUser><br></br>
-      <CounterClass></CounterClass>
-      <ConditoanlRendering></ConditoanlRendering> */}
-      {/* <Product></Product> */}
-      {/* <DataSharingParent></DataSharingParent><br></br>
-      <StudentDetails></StudentDetails>
-      <ProductSearch></ProductSearch>
-      <UseRefDmo></UseRefDmo><br></br> */}
-      {/* <UseRefDemo1></UseRefDemo1><br></br> */}
-      {/* <UseRefDemo3></UseRefDemo3><br></br>
-      <UseRefDemo2></UseRefDemo2> */}
-      {/* <UseRefDemo4></UseRefDemo4><br></br>
-      <ProductUseRef></ProductUseRef> */}
+      <CounterClass></CounterClass><br></br>
+      <ConditoanlRendering></ConditoanlRendering> <br></br>
+       <Product></Product> <br></br>
+       <DataSharingParent></DataSharingParent><br></br>
+      <StudentDetails></StudentDetails><br></br>
+      <ProductSearch></ProductSearch><br></br>
+      <UseRefDmo></UseRefDmo><br></br> 
+       <UseRefDemo1></UseRefDemo1><br></br> 
+      <UseRefDemo3></UseRefDemo3><br></br>
+      <UseRefDemo2></UseRefDemo2> <br></br>
+       <UseRefDemo4></UseRefDemo4><br></br>
+      <ProductUseRef></ProductUseRef> <br></br>
       <TODo></TODo>
 
     </div>
